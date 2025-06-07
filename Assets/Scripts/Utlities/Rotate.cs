d88670107928569e9dd02493a6a261e1c5a260b3
BTevfikAkkeser <buraktevfikakkeser@posta.mu.edu.tr>
2025-06-07 14:10:47 +0300

    pushed_all

---
+++ Assets/Scripts/Utlities/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    public int X =5;
+    public int Y =3 ;
+    public int Z = 5;
+   
+    public void Update()
+    {
+        transform.Rotate(Vector3.right, X*Time.deltaTime); 
+        transform.Rotate(Vector3.up, Y * Time.deltaTime);
+        transform.Rotate(Vector3.down, Z * Time.deltaTime);
+    }
+}
+
